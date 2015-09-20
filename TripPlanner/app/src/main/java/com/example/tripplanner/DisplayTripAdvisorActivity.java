04e2e43e4690d6ba2eb0042c506a5e2d9bb44af8
unknown <luke.lones@gmail.com>
2015-09-19 19:55:33 -0500

    2nd page gets location_id

--- TripPlanner/app/src/main/java/com/example/tripplanner/DisplayTripAdvisorActivity.java
+++ TripPlanner/app/src/main/java/com/example/tripplanner/DisplayTripAdvisorActivity.java
@@ -1,24 +1,63 @@
 package com.example.tripplanner;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
 
 public class DisplayTripAdvisorActivity extends Activity {
     ListView mainListView;
     ArrayAdapter mArrayAdapter;
     ArrayList mNameList = new ArrayList();
-
+    TextView mainTextView;
+    int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_trip_advisor);
+        Intent intent = getIntent();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Api.API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        TripAdvisor tripAdvisor = retrofit.create(TripAdvisor.class);
+        intent.getStringExtra("id");
+        String API_KEY = getResources().getString(R.string.TripAdvisorKey);
+        Call<Api.SearchResults> call = tripAdvisor.searchGeos("San%20Francisco", API_KEY);
+        mainTextView = (TextView) findViewById(R.id.text_test);
+        call.enqueue(new Callback<Api.SearchResults>() {
+            @Override
+            public void onResponse(Response<Api.SearchResults> response) {
+                Api.SearchResults searchResults = response.body();
+                id = searchResults.geos.get(0).location_id;
+                System.out.println("OMG ID = " + id);
+                System.out.println(searchResults.geos.get(0).location_string);
+                mainTextView.setText("OurPlace = " + id);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                t.printStackTrace();
+            }
+        });
+
+
+
     }
 
     @Override
