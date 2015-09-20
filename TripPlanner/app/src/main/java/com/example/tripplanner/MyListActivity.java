b8600ef3025b7d7c72283c1c083d8f1e03411866
rmarinov <chessmasterrado@gmail.com>
2015-09-19 23:58:15 -0500

    New class and update for layout

--- TripPlanner/app/src/main/java/com/example/tripplanner/MyListActivity.java
+++ TripPlanner/app/src/main/java/com/example/tripplanner/MyListActivity.java
@@ -12,7 +12,20 @@ public class MyListActivity extends ListActivity {
         String[] values = new String[] { "Bob's Pizza", "City Park", "Billy Bowling",
                 "Saloon", "Night Club", "Art Museum", "Zoo", "Botanical Garden",
                 "Sally's Steakhouse", "Cindy's Cafe" };
-        // use your custom layout
+        int[] locNums = new int[] {1,2,3,4,5,6,7,8,9,0};
+        Boolean[] locSel = new Boolean[] {false,false,false,false,false,false,false,false,false,false};
+
+
+
+
+
+        displayLocation[] disVals = new displayLocation[]{};
+
+
+        for(int i=0; i < values.length; i++){
+            //disVals;
+        }
+
         MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this, values);
         setListAdapter(adapter);
     }
